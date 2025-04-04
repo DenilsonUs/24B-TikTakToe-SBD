65aec8029e4c506bf8c7b2738abd7393823efedc
Denilson <urias4141@gmail.com>
2025-04-04 13:57:34 +0100

    JS
    Get the user name and show in the Dom

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+document
+  .getElementById("playerForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent form refresh
 
+    const player1Name = document.getElementById("player-name1").value;
+    const player2Name = document.getElementById("player-name2").value;
+
+    // Display the names on screen
+    document.getElementById(
+      "playerDisplay"
+    ).textContent = `Player 1: ${player1Name}, Player 2: ${player2Name}`;
+  });
